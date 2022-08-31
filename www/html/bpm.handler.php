426610d173b5040513bedd307a21422871e5986c
Angutivik Casper Rúnur Tausen <runurth@gmail.com>
2022-08-31 09:56:06 +0200

    some changes to handler

--- www/html/bpm.handler.php
+++ www/html/bpm.handler.php
@@ -1,4 +1,31 @@
 <?php
 
-$data = file_get_contents('http://192.168.80.12:8080/bpm');
+/*
+ * Servers:
+ * - Skúlaheim
+ *   - 192.168.80.2
+ *   - 192.168.80.12
+ *   - 192.168.80.17
+ * - Hotspot
+ *   - 192.168.95.115
+ * - Heima
+ *   - 192.168.1.222
+ * - Skúli
+ *   - 10.135.16.54
+ */
+ $server = '10.135.16.54';
+
+$q = $_REQUEST["q"];
+
+$data = "";
+
+if ($q !== "") {
+	$q = strtolower($q);
+	if ($q == "bpm") {
+		$data = file_get_contents("http://$server:8080/bpm");
+	} else {
+		$data = "No result";
+	}
+}
+
 echo $data;
