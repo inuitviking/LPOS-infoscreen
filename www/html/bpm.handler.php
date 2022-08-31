871cb0608d1e95dd270cc2a13059fcde92716f2b
Angutivik Casper Rúnur Tausen <runurth@gmail.com>
2022-08-31 10:58:00 +0200

    asdf

--- www/html/bpm.handler.php
+++ www/html/bpm.handler.php
@@ -22,14 +22,12 @@ $data = "";
 if ($q !== "") {
 	$q = strtolower($q);
 	if ($q == "bpm") {
-		$data = file_get_contents("http://localhost:8080/bpm");
-		$data = json_decode($data);
+//		$data = file_get_contents("http://localhost:8080/bpm");
+//		$data = json_decode($data);
 		$data = shell_exec('ping -c1 google.com');
 	} else {
 		$data = "No result";
 	}
 }
 
-echo "<pre>";
-print_r($data);
-echo "</pre>";
+echo $data;
