f22ef531e172cf1b92f95f65f856f94d77c6e8d2
Angutivik Casper Rúnur Tausen <runurth@gmail.com>
2022-09-03 16:11:41 +0200

    hopefully smarter templating

--- www/html/bpm.handler.php
+++ www/html/bpm.handler.php
@@ -50,8 +50,8 @@ $alertStatus = 'alert-danger';
 			default => 'alert-danger',
 		};
 
-		?>	<div class="col-md-4 col-gap bed alert <?php echo $alertStatus ?>"
-		style="margin: 0 20px 0 20px;">
+		?>	<div class="col-md-3 bed alert <?php echo $alertStatus ?>"
+		style="margin: 20px;">
 			<p class="fw-bold fs-5"><?php echo str_replace('_', ' ', $bed['bed']) ?></p>
 			<p">BPM: <span class="fs-41"><?php echo $bed['bpm'] ?></span></p>
 			<p>Call: <?php echo $bed['call']; ?></p>
