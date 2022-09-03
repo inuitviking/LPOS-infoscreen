c2db304826db952c46f14e67d194804d9d1dca41
Angutivik Casper Rúnur Tausen <runurth@gmail.com>
2022-09-03 16:46:24 +0200

    Fixed the spacing betweenthingies

--- www/html/bpm.handler.php
+++ www/html/bpm.handler.php
@@ -54,9 +54,9 @@ $alertStatus = 'alert-danger';
 		};
 
 		?>	<div class="alert <?php echo $alertStatus ?>"
-		style="margin-left: 4.166px;
-			margin-right: 4.166px;
-			width: calc( 100% / 4 );;">
+		style="margin-left: 4px;
+			margin-right: 4px;
+			width: calc( 100% / 3 - 8px );">
 			<p class="fw-bold fs-5"><?php echo str_replace('_', ' ', $bed['bed']) ?></p>
 			<p">BPM: <span class="fs-41"><?php echo $bed['bpm'] ?></span></p>
 			<p>Call: <?php echo $bed['call']; ?></p>
